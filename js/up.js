99e417814e6bb71d2d8547b7889dd46216ce4246
pascoal zage <138582742+Pascoal54@users.noreply.github.com>
2024-10-29 10:33:03 +0100

    Add files via upload

---
+++ js/up.js
@@ -0,0 +1,5 @@
+document.querySelector(".btn-cta").addEventListener("click", function() {
+    alert("🎉 Parabéns! Sua mentoria 'Minha Primeira Venda 0.7 Dígitos' foi adicionada com sucesso ao seu pedido. 🚀 Prepare-se para transformar suas vendas online! Em instantes, você será direcionado para o WhatsApp para finalizar a inscrição.");
+
+  });
+  
